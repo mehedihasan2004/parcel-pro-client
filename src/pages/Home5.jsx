400c71d0dd59e3961feb2020944259320daa8d0a
Abuhurayratouhid <abuhurayratouhid@gmail.com>
2023-02-18 11:38:27 +0600

    some changes

--- src/pages/Home5.jsx
+++ src/pages/Home5.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home5 = () => {
-  return <div> Abu hurayra touhid another push</div>;
+  return <div> Abu hurayra touhid another push Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem deserunt nobis fuga quisquam tempora id totam necessitatibus aspernatur est laudantium voluptatum repellat commodi, cumque excepturi cupiditate inventore labore sit dicta?</div>;
 };
 
 export default Home5;
