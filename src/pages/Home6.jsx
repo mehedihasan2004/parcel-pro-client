5e04b728765796fca208d0c7a662fa6b29f54006
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-02-18 12:17:22 +0600

    It push frm main branch

--- src/pages/Home6.jsx
+++ src/pages/Home6.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Home6 = () => {
-  return <div></div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam dolor
+      tempora deleniti numquam adipisci voluptatum eaque maxime quos, ad,
+      voluptatibus, quam incidunt dolorem. Ipsa, dolore minus? Neque laboriosam
+      nulla impedit?
+    </div>
+  );
 };
 
 export default Home6;
