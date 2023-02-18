c7d5cac30e85485f010a21e7c726a52ae6db1e8c
Abuhurayratouhid <abuhurayratouhid@gmail.com>
2023-02-18 23:46:23 +0600

    Edited by Abu hurayra

--- src/pages/home/BestPrice.jsx
+++ src/pages/home/BestPrice.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const BestPrice = () => {
-  return <div>Best Price</div>;
+  return <div>Best Price Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ex libero quas at pariatur! Molestias obcaecati soluta dolorem! Blanditiis repellat ut quos maxime sunt neque unde mollitia, optio dignissimos! Inventore?</div>;
 };
 
 export default BestPrice;
