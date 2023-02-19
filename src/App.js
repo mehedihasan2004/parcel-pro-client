31e7ddb7df6a57582fd13c95fd231f1b294fe9f4
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-02-19 12:02:42 +0600

    baseline removed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import { useRoutes } from "react-router-dom";
 import { Footer, Navbar } from "./components";
 import { Home } from "./pages/home";
-import CssBaseline from "@mui/material/CssBaseline";
-
 const App = () => {
   const routes = useRoutes([
     {
@@ -16,7 +14,6 @@ const App = () => {
       <Navbar />
       {routes}
       <Footer />
-      <CssBaseline />
     </div>
   );
 };
